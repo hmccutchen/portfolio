a662b7718e2904ffb8b4b60999e9e14afbaddcdf
Hassan Mccutchen <mccutchen.hassan@gmail.com>
2020-04-17 16:24:00 -0400

    add dark mode

--- main.js
+++ main.js
@@ -59,16 +59,6 @@ var tag = document.createElement('script');
 
 
 
-
-//animte css animation lib function
-// const flip = document.getElementsByClassName("action");
-
-// const runFlip = (e) => {
-
-//  e.classList.toggle('bounce');
-
-// }
-
 var nav = document.getElementById("nav");
 
 nav.addEventListener("click", function(){
@@ -108,6 +98,68 @@ p.scrollIntoView({behavior: 'smooth', block: 'start'});
 
 })
 
+
+   toggleCheckBox = () => {
+   let input = document.getElementById("color-switch");
+    let pTags = document.querySelectorAll('p');
+    let headerOne = document.querySelectorAll('h1');
+    let headerTwo =  document.querySelectorAll('h2');
+    let navigation = document.querySelector(".navigation");
+    let navigationLinks = document.querySelectorAll('a');
+    let myName = document.getElementById('myName');
+    let footer = document.querySelector('.footer');
+    let cpyRight = document.querySelector('.footer-copyright');
+
+   if(input.checked){
+    document.body.style.backgroundColor = '#1f1b21';
+    navigation.style.backgroundColor = "#1f1b21";
+    myName.style.color = 'white';
+    footer.style.backgroundColor = '#1f1b21';
+    cpyRight.style.color = 'white';
+
+    navigationLinks.forEach((a) => {
+      a.style.color = 'white';
+    })
+
+    pTags.forEach((p)=> {
+      p.style.color = 'white';
+    })
+    headerOne.forEach((h)=> {
+      h.style.color = 'white';
+    })
+
+    headerTwo.forEach((h)=> {
+      h.style.color = 'white';
+      h.style.borderBottom = '2px solid white';
+    })
+
+
+   } else if(input.checked === false){
+
+    document.body.style.backgroundColor = 'white';
+    navigation.style.backgroundColor = "#f7f5f5";
+    myName.style.color = 'black';
+    footer.style.backgroundColor = '#f7f5f5';
+    cpyRight.style.color = 'black';
+
+    navigationLinks.forEach((a) => {
+      a.style.color = 'black';
+    })
+    pTags.forEach((p)=> {
+      p.style.color = 'black';
+    })
+    headerOne.forEach((h)=> {
+      h.style.color = 'black';
+    })
+
+    headerTwo.forEach((h)=> {
+      h.style.color = 'black';
+      h.style.borderBottom = '2px solid black';
+    })
+
+   };
+
+  }
 
 
 
