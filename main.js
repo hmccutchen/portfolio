0d9a9d0a93110888bfd29c0fcde0c70f26dc7070
Hassan McCutchen <hassanmccutchen@Hassans-MBP-2.lan>
2020-09-22 17:53:12 -0400

    created new portfolio version

--- main.js
+++ main.js
@@ -7,66 +7,6 @@
   }
 
 
-var tag = document.createElement('script');
-
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-
-          height: '600',
-          width: '1280',
-          videoId: 'V9NbfpG9tRM',
-
-          disablekb: 0,
-
-          playerVars: { 'controls': 0,
-                        'modestbranding': 1,
-                        'rel': 0,
-                        'autoplay': 1,
-                        color: 'white',
-                        'version': 3,
-                        'start': 960,
-                        'end': 1200,
-                        'theme': 'light',
-                        'loop': 1,
-                        'playsinline': 1,
-                        'disablekb': 1,
-          },
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-        event.target.setVolume(0);
-
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          // setTimeout(stopVideo, 49500);
-          done = true;
-        }
-      }
-
-
-
-
 var nav = document.getElementById("nav");
 
 nav.addEventListener("click", function(){
